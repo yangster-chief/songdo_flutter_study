7426e7ecea8a00c092fd1b898ebd67427647a0ff
thunder-ramgee <38216680+thunder-ramgee@users.noreply.github.com>
2022-09-19 18:24:11 +0900

    add button

--- lib/page/stopwatch_page.dart
+++ lib/page/stopwatch_page.dart
@@ -7,14 +7,90 @@ import 'package:flutter/material.dart';
 ///
 /// Description:
 ///
-class StopwatchPage extends StatelessWidget {
+class StopwatchPage extends StatefulWidget {
   const StopwatchPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Text(
-      'stopwatch',
-    ));
+  State<StopwatchPage> createState() => _StopwatchPageState();
+}
+
+class _StopwatchPageState extends State<StopwatchPage> {
+  bool _isRunning = false;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Text(
+                  '00:00',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {
+                    _onReset();
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      color: Colors.lightBlueAccent,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(Icons.autorenew),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    _onPressed();
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(32),
+                    decoration: const BoxDecoration(
+                      color: Colors.blueAccent,
+                      shape: BoxShape.circle,
+                    ),
+                    child: _isRunning
+                        ? const Icon(Icons.pause)
+                        : const Icon(Icons.play_arrow),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    _onRecord();
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      color: Colors.lightBlue,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(Icons.edit),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 32),
+          ],
+        ),
+      );
+
+  void _onPressed() {
+    setState(() {
+      _isRunning = !_isRunning;
+    });
+  }
+
+  void _onReset() {
+    print('reset');
+  }
+
+  void _onRecord() {
+    print('record');
   }
 }
