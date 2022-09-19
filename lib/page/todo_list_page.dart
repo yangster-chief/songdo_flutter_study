17422cb920f9e00c0f12608ee53b57b49e8fb080
thunder-ramgee <38216680+thunder-ramgee@users.noreply.github.com>
2022-09-19 15:35:53 +0900

    add listbuilder

--- lib/page/todo_list_page.dart
+++ lib/page/todo_list_page.dart
@@ -7,13 +7,37 @@ import 'package:flutter/material.dart';
 ///
 /// Description:
 ///
-class TODOListPage extends StatelessWidget {
+class TODOListPage extends StatefulWidget {
   const TODOListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TODOListPage> createState() => _TODOListPageState();
+}
+
+class _TODOListPageState extends State<TODOListPage> {
+  List<String> todoList = [
+    'Flutter study 1주차 숙제 하기',
+    'Flutter docs 정독하기',
+    'FlutterFire 적용',
+  ];
+
+  @override
+  Widget build(BuildContext context) => ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (BuildContext context, int index) =>
+            _ListItem(item: todoList[index]),
+      );
+}
+
+class _ListItem extends StatelessWidget {
+  const _ListItem({Key? key, required this.item}) : super(key: key);
+
+  final String item;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('todo list'),
+      child: Text(item),
     );
   }
 }
