ddf70ad38df051b8460edc9d36153c99048c15e8
thunder-ramgee <38216680+thunder-ramgee@users.noreply.github.com>
2022-09-19 13:14:54 +0900

    stateful 위젯으로 변환

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/counter_page.dart';
+import 'package:flutter_study/page/counter_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
